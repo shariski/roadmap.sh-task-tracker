51a4351a36a2ce99e5899daf54e1ede24bf1a4c1
shariski <falahudin@bullionecosystem.com>
2025-10-15 13:03:12 +0700

    feat: list tasks

--- main.go
+++ main.go
@@ -9,39 +9,74 @@ import (
 )
 
 type Task struct {
-	Id string `json:"id"`;
-	Description string `json:"description"`;
-	Status string `json:"status"`;
-	CreatedAt time.Time `json:"createdAt"`;
-	UpdatedAt time.Time `json:"updatedAt"`;
+	Id          string    `json:"id"`
+	Description string    `json:"description"`
+	Status      string    `json:"status"`
+	CreatedAt   time.Time `json:"createdAt"`
+	UpdatedAt   time.Time `json:"updatedAt"`
 }
 
 func main() {
-	// index 0 is path of the binary
-	fmt.Println(os.Args[1:])	
-	tasks := getTasks("ALL")
-	fmt.Println(tasks)
+	if len(os.Args) < 2 {
+		fmt.Println("please insert command")
+		return
+	}
+
+	command := os.Args[1]
+	if command == "list" {
+		status := "ALL"
+		if len(os.Args) > 2 {
+			status = os.Args[2]
+			if status != "done" && status != "todo" && status != "in-progress" {
+				fmt.Println("please fill valid status")
+				return
+			}
+		}
+		tasks := getTasks(status)
+		for _, task := range tasks {
+			fmt.Println(task)
+		}
+		return
+	}
 }
 
 func getTasks(status string) []Task {
-	fmt.Println(status)
-	file, err := os.ReadFile("tasks.json")
-	if err != nil {
-		if errors.Is(err, os.ErrNotExist) {
-			err = os.WriteFile("tasks.json", []byte("[]"), 0644)
-			if err != nil {
-				panic(err)
-			}
-		} else {
-			panic(err)
-		}
-	}
-	var tasks []Task
-	if file != nil {
-		err = json.Unmarshal(file, &tasks)
-	}
+	allTasks, err := safeReadFile()
 	if err != nil {
 		panic(err)
 	}
-	return tasks 
+	if status == "ALL" {
+		return allTasks
+	}
+
+	var filteredTasks []Task
+	for _, task := range allTasks {
+		if task.Status == status {
+			filteredTasks = append(filteredTasks, task)
+		}
+	}
+
+	return filteredTasks
+}
+
+func safeReadFile() ([]Task, error) {
+	filePath := "tasks.json"
+
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			if err := os.WriteFile(filePath, []byte("[]"), 0644); err != nil {
+				return nil, fmt.Errorf("failed to create %s: %w", filePath, err)
+			}
+			return []Task{}, nil
+		}
+		return nil, fmt.Errorf("failed to read %s: %w", filePath, err)
+	}
+
+	var tasks []Task
+	if err := json.Unmarshal(data, &tasks); err != nil {
+		return nil, fmt.Errorf("failed to parse %s: %w", filePath, err)
+	}
+
+	return tasks, err
 }
